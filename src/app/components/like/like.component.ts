18c220fddd4ddf943d77979e3d52fd78daf5f9c3
Ahlem Gammoudi <92860662+AhlemGammoudi79@users.noreply.github.com>
2022-01-24 21:01:46 +0100

    Update like.component.ts

--- src/app/components/like/like.component.ts
+++ src/app/components/like/like.component.ts
@@ -7,28 +7,9 @@ import { GiftService } from 'src/app/services/gift.service';
   styleUrls: ['./like.component.css']
 })
 export class LikeComponent implements OnInit {
-likes:any;
-gifts:any;
-connectedUser:any;
-  constructor(private giftService:GiftService) { }
 
-  ngOnInit() {
-    this.connectedUser = JSON.parse(localStorage.getItem("connectedUser"));
 
-    this.likes = JSON.parse(localStorage.getItem("likes"));
-console.log(this.likes);
-this.giftService.getGifts().subscribe((data) => {
-  if (this.connectedUser) {
-    
   
-  console.log(data.gifts);
-  this.gifts = data.gifts;
+
 
   }
- 
-})
-
-  
-
-
-  }}
